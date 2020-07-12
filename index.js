a47d70dc1252112440cb5415b8db46fdabd1f6c8
Jordan Hawker <hawker.jordan@gmail.com>
2020-07-12 14:39:54 -0700

    Inject font dynamically

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  contentFor(type) {
+    if (type === 'head') {
+      return '<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital@0;1&family=Playfair+Display+SC:wght@900&family=Playfair+Display:ital,wght@0,800;1,800&family=Manrope:wght@800&display=swap" rel="stylesheet">';
+    }
+  }
 };
