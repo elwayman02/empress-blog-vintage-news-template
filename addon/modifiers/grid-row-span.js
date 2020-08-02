71a68ffca3bc4b96d992db717a073b8c819b8056
Jordan Hawker <hawker.jordan@gmail.com>
2020-08-02 16:43:54 -0700

    Handle image loading

--- addon/modifiers/grid-row-span.js
+++ addon/modifiers/grid-row-span.js
@@ -1,10 +1,27 @@
 import { modifier } from 'ember-modifier';
 
-export default modifier(function gridRowSpan(element/*, params, hash*/) {
-  let image = element.getElementsByClassName('post-card-image')[0];
-  let imageHeight = image ? image.offsetHeight : 0;
+export default modifier(function gridRowSpan(element, params, hash) {
   let contentHeight = element.getElementsByClassName('post-card-content')[0].offsetHeight;
-  let numRows = Math.ceil((contentHeight + imageHeight) / (25 + 5));
+  if (hash.image) {
+    let imageWrapper = element.getElementsByClassName('post-card-image')[0];
+    let image = imageWrapper.getElementsByTagName('img')[0];
+    if (image.complete) {
+      let imageHeight = imageWrapper.offsetHeight;
+      let numRows = Math.ceil((contentHeight + imageHeight) / (25 + 5));
 
-  element.style.gridRowEnd = `span ${numRows}`;
+      element.style.gridRowEnd = `span ${numRows}`;
+    } else {
+      image.addEventListener('load', function () {
+        let loadedImageWrapper = element.getElementsByClassName('post-card-image')[0];
+        let imageHeight = loadedImageWrapper.offsetHeight;
+        let numRows = Math.ceil((contentHeight + imageHeight) / (25 + 5));
+
+        element.style.gridRowEnd = `span ${numRows}`;
+      });
+    }
+  } else {
+    let numRows = Math.ceil(contentHeight / (25 + 5));
+
+    element.style.gridRowEnd = `span ${numRows}`;
+  }
 });
